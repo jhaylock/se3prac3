1ee2d8c2b5750e0beaace265708f1bd8285bd0c2
hayl0017 <hayl0017@flinders.edu.au>
2014-04-03 15:00:25 +1030

    tests added for tilting right

--- test.c
+++ test.c
@@ -31,6 +31,12 @@ int ttl_vector(int i1,int i2,int i3,int i4,char *msg,
   return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_left);
 }
 
+int ttr_vector(int i1,int i2,int i3,int i4,char *msg,
+               int o1,int o2,int o3,int o4)
+{
+  return line_vector_test(i1,i2,i3,i4,msg,o1,o2,o3,o4,tilt_line_right);
+}
+
 int test_tilt_left()
 {
   int e=0;
@@ -47,6 +53,20 @@ int test_tilt_left()
   e|=ttl_vector(0,1,0,1,"Move left without combining",1,1,0,0);
   e|=ttl_vector(1,0,0,1,"Move left without combining",1,1,0,0);
   return e;
+}
+
+int test_tilt_right()
+{
+  int e=0;
+  e|=ttr_vector(0,0,0,0,"Empty list is empty after shift",0,0,0,0);
+  e|=ttr_vector(0,0,0,1,"Value on right stays on right after shift",0,0,0,1);
+  e|=ttr_vector(1,0,0,0,"Value on left shifts to right edge after shift",0,0,0,1);
+  e|=ttl_vector(0,1,0,0,"Value in middle shifts to right edge after shift",0,0,0,1);
+  e|=ttl_vector(1,2,4,8,"Distinct values don't combine",1,2,4,8);
+  e|=ttl_vector(1,1,1,1,"Combinations don't cascade",0,0,2,2);
+  e|=ttl_vector(0,0,1,1,NULL,0,0,0,2);
+  e|=ttl_vector(4,0,1,1,NULL,0,0,4,2);
+  e|=ttl_vector(2,0,1,1,NULL,0,0,2,2);
 }
 
 int main(int argc,char **argv)
